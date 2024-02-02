3edee103f1103988ce11dd7763d6f3fdf19795fa
Olivier <omalahel@gmail.com>
2024-02-02 15:35:43 +0100

    update front

--- components/Project.js
+++ components/Project.js
@@ -1,9 +1,11 @@
 import styles from "../styles/Project.module.css";
 import mymoviz from "../public/mymoviz_project.png";
 import Ecna from "../public/ECNA.jpeg";
-import locapic from '../public/Locapic.jpeg'
+import locapic from "../public/Locapic.jpeg";
+import weatherApp from "../public/WeatherApp.jpg";
 import ImageSlider from "./ImageSlider";
 import { IoEarth } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa";
 
 function Project() {
   const IMAGES = [
@@ -33,7 +35,31 @@ function Project() {
       description:
         "Locapic is a project which was made in React Native, at La Capsule Bootcamp. In this app you're be able to put a pin to save the differents places in the map of France",
       src: locapic,
-    }
+    },
+    {
+      title: "WeatherApp",
+      description:
+        "A little web application in using an API to have the weather in differents towns. You can find it on my GitHub",
+      src: weatherApp,
+      link: (
+        <div>
+          <a href="https://github.com/OliverMunian/WeatherApp_frontend">
+            {" "}
+            <p className={styles.iconlink}>
+              {" "}
+              Frontend <FaGithub />
+            </p>
+          </a>
+          <a href="https://github.com/OliverMunian/WeatherApp_Backend">
+            {" "}
+            <p className={styles.iconlink}>
+              {" "}
+              Backend <FaGithub />
+            </p>
+          </a>
+        </div>
+      ),
+    },
   ];
   return (
     <div className={styles.main} id="Project">
