fac43807887a5e928174879ec204b9d0f68650ae
Olivier <omalahel@gmail.com>
2024-02-16 11:29:47 +0100

    update responsive

--- components/Video.js
+++ components/Video.js
@@ -1,11 +1,22 @@
 import styles from "../styles/video.module.css";
 import Video from "next-video";
 import fpvdrone from "../videos/FPV_drone.mp4";
+import MuxPlayer from "@mux/mux-player-react";
 
 function video() {
   return (
     <div className={styles.main}>
-      <Video src={fpvdrone} autoplay loop muted primary-color='#f2b129' nofullscreen/>
+      {/* <Video src={fpvdrone} autoplay loop muted primary-color='#f2b129' controlslist='nofullscreen'/> */}
+      <MuxPlayer
+        streamType="on-demand"
+        playbackId="qw9wmwBxy6xJtQMNTr00vAS7pDhfujYeIgBpiqTZfoe4"
+        metadataViewerUserId="Placeholder (optional)"
+        primaryColor="transparent"
+        secondaryColor="transparent"
+        autoPlay
+        loop
+        muted
+      style={{'--controls': 'none'}}/>
       <div className={styles.comments}>
         <h1>FPV Drone</h1>
         <p>Training session - Quick flight outside</p>
