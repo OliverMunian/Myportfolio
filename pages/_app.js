8c9c0d70a4234024a25e0a210a34d823c186d845
Olivier <omalahel@gmail.com>
2024-10-31 07:15:30 +0100

    update front

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,15 @@ function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Olivier Malahel</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Outfit&display=swap"
+          rel="stylesheet"
         />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
